e0cb12ef0e3bf2588f08e1962fa3849f5b10888e
nurlasova <nurlasova.kg@gmail.com>
2022-07-12 06:19:54 +0600

    chan

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,10 +4,10 @@ import './_app.scss';
 import { Route, Switch } from 'react-router';
 import CartPage from '../../pages/cart/cart-page';
 import HomePage from '../../pages/home/home-page';
+
+
 // import BaseModalWrapper  from '../../components/modal/BaseModalWrapper';
 
-
-const App: React.FC = () => {
   return (
     <div className="app__wrapper container">
       <div className="app__container">
@@ -21,7 +21,6 @@ const App: React.FC = () => {
       </div>
     </div>
   );  
-};
 
 
 export default App;
